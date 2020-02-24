ab6f9f8c7272868dc71193fd0c81daf8f2dcb214
Junaid Farooq <junaid@evercam.io>
2020-02-24 12:44:05 +0500

    Added routes.

---
+++ routes/flightDetailsRoutes.js
@@ -0,0 +1,20 @@
+ 'use strict';
+module.exports = function(app) {
+ let flightDetailsCtrl = require('../controllers/flightDetailsController');
+
+  app
+    .route('/flight-details')
+    .post(flightDetailsCtrl.create);
+
+  app
+    .route('/flight-details')
+    .get(flightDetailsCtrl.show);
+
+  // app
+  //   .route('/flight-details/:id')
+  //   .put(flightDetailsCtrl.update);
+
+  // app
+  //   .route('/flight-details/:id')
+  //   .delete(flightDetailsCtrl.delete);
+};
